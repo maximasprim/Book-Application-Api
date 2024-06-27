bac2d4e348705cd3b89e80c7a007282f2c605e1a
maximasprim <michaelmwasame6@gmail.com>
2024-06-27 14:25:43 +0300

    updated schema

--- src/drizzle/migrations/0000_blue_puck.sql
+++ src/drizzle/migrations/0000_blue_puck.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS "books" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"title" varchar(255),
 	"Author" varchar(20),
-	"published_at" timestamp DEFAULT now()
+	"year" varchar(4)
 );
--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -2,6 +2,7 @@
 
 import { pgTable, serial, varchar,timestamp } from "drizzle-orm/pg-core"
 import { primaryKey } from "drizzle-orm/mysql-core";
+// import { string } from "zod";
 
 
 
@@ -10,7 +11,7 @@ export const booksTable=pgTable("books",{
     id: serial("id").primaryKey(),
     title: varchar("title", {length: 255}),
     author: varchar("Author",{length: 20}),
-    year: timestamp("published_at").defaultNow()
+    year: varchar("year",{length: 4}),
 })
 
 export type TIBooks = typeof booksTable.$inferInsert
